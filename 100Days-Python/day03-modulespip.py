d67ace5ba18a459a784cbca69c92825b75a89df8
Jishank Rimal <thejishank@gmail.com>
2024-09-10 09:50:25 +0545

    Add files via upload

--- 100Days-Python/day03-modulespip.py
+++ 100Days-Python/day03-modulespip.py
@@ -1,6 +1,6 @@
-import pandas  # This is an example of external module
-import hashlib  # This is an example of built in module
+import pandas as pd # This is an example of external module
+import sys # This is an example of built in module
 
-print("Hi")
-
+print(pd.__version__)
+print(sys.version)
 
