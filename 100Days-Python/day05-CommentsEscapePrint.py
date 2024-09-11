3b390e7550051d1207c198981c67365d96e9fc58
Jishank Rimal <thejishank@gmail.com>
2024-09-11 14:16:56 +0545

    Add files via upload

---
+++ 100Days-Python/day05-CommentsEscapePrint.py
@@ -0,0 +1,36 @@
+# Hey Jishank, Please dont remove this line
+# Author: Jishank
+# Course: Day05 of 100
+print("Hey I am a  good boy\nand this viewer is also a good boy/girl")
+
+# This above was single line comment and below is milti-line comment
+
+""" Hey Jishank, Please dont remove this line
+Author: Jishank
+Course: Day05 of 100 """
+print("Hey I am a  good boy\nand this viewer is also a good boy/girl")
+
+# ESCAPE SEQUENCE CHARACTERS
+
+# to print in new line we use - \n
+
+print("Hello world\nHow are you?") 
+
+# to use characters which is also in syntax we use backslash 
+
+print("Hello, this boy \"Jishank\" is good boy")
+
+# Parameters
+# 1 - sep=""
+# To join any string in print function
+
+print("Hey", "i", "am", "the", "jishank", 1, sep = "-")
+
+
+# Parameters
+# 2 - end = ""
+# To join any string in print function
+
+print("Hey", "i", "am", "the", "jishank", 1, sep = "-", end=" ")
+print("Haha")
+
