8f72cf840777444e35394a1cf7aab22f8b4e7ba1
Jishank Rimal <thejishank@gmail.com>
2024-09-11 14:17:17 +0545

    Add files via upload

---
+++ 100Days-Python/day06-VariablesDataTypes.py
@@ -0,0 +1,33 @@
+a = 11
+b = True
+c = "True"
+d = "11"
+e = None
+f = complex(3, 4)
+
+print("Type of a = ", type(a))
+print("Type of b = ", type(b))
+print("Type of c = ", type(c))
+print("Type of d = ", type(d))
+print("Type of e = ", type(e))
+print("Type of f = ", type(f))
+
+
+# Sequenced data: list, tuple
+
+# 1. list
+
+list1 = [8, 2.3, [-4,5], ["apple", "banana"], 5, ("hi")]
+print(list1)
+
+# 2. tuple
+
+tuple1 = (("parrot", "sparrow"), ("lion", "Tiger"), ["bike", "scooter"])
+print(tuple1)
+
+
+
+# Mapped data: dict
+
+dict1 = {"Name": "Sakshi", "Age":20, "canVote":True}
+print(dict1)
