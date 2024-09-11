3841ad57f07d12f54e3eeb2666cad809b5491e15
Jishank Rimal <thejishank@gmail.com>
2024-09-11 14:17:39 +0545

    Add files via upload

---
+++ 100Days-Python/day07-CreateaCalculator.py
@@ -0,0 +1,19 @@
+""" Create a calculator capable of performing addition, subtraction, multiplication 
+    and division operations on two numbers. Your program should format the output in 
+    a readable manner! """
+
+
+m = 20
+n = 10
+
+cal1 = m+n
+print("Sum of", m,"and", n," is:", cal1)
+
+cal2 = m-n
+print("Difference of", m,"and", n,"is", cal2)
+
+cal3 = m*n
+print("Multiplication of", m,"and", n," is:", cal3)
+
+cal4 = m/n
+print("Difference of", m,"and", n,"is", cal4)
